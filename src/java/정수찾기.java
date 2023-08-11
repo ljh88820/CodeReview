3dccbb163354462f7af96b0979d99f95fd35b92d
ljh88820 <ljh88820@naver.com>
2023-08-11 13:21:57 +0900

    [UPDATE] JAVA - 정수찾기

---
+++ src/java/정수찾기.java
@@ -0,0 +1,14 @@
+package java;
+
+public class 정수찾기 {
+	 public int solution(int[] num_list, int n) {
+	        int answer = 0;
+	        
+	        for(int i = 0 ; i < num_list.length ; i++) {
+	        	if(num_list[i] == n) {
+                     answer = 1;
+	        	}//if
+	        }//for
+	        return answer;
+	    }//soulution
+}//end class
